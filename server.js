81191cdca678a993646a6d957d35c33a86cc359c
Manisha Solanki <msolanki7@mySeneca.ca>
2022-10-14 12:48:02 -0400

    final commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Manisha Gautam Solanki Student ID: 162281216 Date: 30-09-2022
+*  Name: Manisha Gautam Solanki Student ID: 162281216 Date: 14-10-2022
 *
 *  Online (Cyclic) Link:  https://orchid-kingfisher-yoke.cyclic.app
 *
@@ -11,10 +11,21 @@
 const express = require('express');
 const path = require('path');
 const service = require('./blog-service');
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
 
 const app = express();
 
 const port = process.env.port || 8080;
+cloudinary.config({
+    cloud_name: 'dkq4ndvbs',
+    api_key: '738747766931337',
+    api_secret: 'fFq48RpS7qwt0A90JuVfrMkZuW4',
+    secure: true
+});
+
+const upload = multer(); 
 
 app.use(express.static('public'));
 
@@ -26,23 +37,88 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/about.html'));
 })
 
+
+
 app.get('/blog', (req, res) => {
     service.getPublishedPosts().then(data => res.json(data)).catch(err => res.json(err));
 })
 
-app.get('/posts', (req, res) => {
-    service.getAllPosts().then(data => res.json(data)).catch(err => res.json(err));
+app.get('/post/:value', (req, res) => {
+    service.getPostById(req.params.value).then(data => res.send(data)).catch(err => res.json(`message: ${err}`));
 })
+
+app.get('/posts', (req, res) => {
+    if (req.query.category) {
+        service.getPostsByCategory(req.query.category).then(data => res.send(data)).catch(err => res.json(`message: ${err}`));
+    } else if (req.query.minDate) {
+        service.getPostsByMinDate(req.query.minDate).then(data => res.send(data)).catch(err => res.json(`message: ${err}`));
+    } else {
+        service.getAllPosts().then(data => res.send(data)).catch(err => res.json(`message: ${err}`));
+    }
+})
+
+
 
 app.get('/categories', (req, res) => {
     service.getCategories().then(data => res.json(data)).catch(err => res.json(err));
 })
 
-app.use((req, res) => {
-    res.status(404).send("Page Not Found");
+app.get('/posts/add', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/addPost.html'));
 })
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
     service.initialize().then((data) => console.log(data)).catch((err) => console.log(err));
+})
+
+
+
+// Adding POST routes
+app.post('/posts/add', upload.single("featureImage"), (req, res) => {
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processPost(uploaded.url);
+        });
+    } else {
+        processPost("");
+    }
+
+    function processPost(imageUrl){
+        req.body.featureImage = imageUrl;
+
+        const postData = {
+            "body": req.body.body,
+            "title": req.body.title,
+            "postDate": new Date().toISOString().split('T')[0],
+            "category": req.body.category,
+            "featureImage": imageUrl,
+            "published": req.body.published,
+        }
+
+        service.addPost(postData).then(data => res.redirect('/posts')).catch(err => res.json(`message: ${err}`));
+    }
+
 })
